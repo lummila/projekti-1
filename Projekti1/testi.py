6a29cd3676076741fb699edac85a46241b618c8c
lummila <lummila.aleksi@gmail.com>
2023-09-19 16:31:25 +0300

    projektin aloitus

--- Projekti1/testi.py
+++ Projekti1/testi.py
@@ -1,8 +1,28 @@
-import _mysql_connector
+import mysql.connector
 import random
 import math
 import geopy
 
 
-def distance(start, dest):
-    return distance.distance(start, dest).km
+def checkForDist(start, dest):
+    return geopy.distance.distance(start, dest).km
+
+
+def travelPrice(start, dest):
+    distance = checkForDist(start, dest)
+    return int(distance / 100)
+
+
+def sqlDistanceQuery(start, dest):
+    pointer = connection.cursor()
+    return
+
+
+connection = mysql.connector.connect(
+    host="127.0.0.1",
+    port=3306,
+    database="flight_game",
+    user="root",
+    password="metropolia",
+    autocommit=True
+)
