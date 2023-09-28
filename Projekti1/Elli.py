1cd9a44a3c9f76bc527d846a673b30595bbb082d
elliriskala <elli.riskala@hotmail.com>
2023-09-28 12:50:29 +0300

    aaaaaaaaa

--- Projekti1/Elli.py
+++ Projekti1/Elli.py
@@ -1,1 +1,15 @@
-# testi
+# coincidences
+# positive:
+# "Nice! You found a 100€ bill on the airport floor." 100e will be added to your account
+# "You were helpful to a lost elderly. For the kind act he rewarded you with a 50€ bill!" 50e will be added to your account
+# "There was a free seat at a more eco-friendly airplane. 20kg was removed from your emissions!" 20kg of emissions will be removed
+# "Lucky you! The flight company made a mistake with your tickets. You'll be getting 80€ cashback!" 80e will be added to your account
+# "The airplane took a shorter route. Emissions were 10kg less than expected." 10kg of emissions will be removed
+
+# negative
+# "The airport lost your luggage... You'll have to wait one night at the airport." one turn is used
+# "Your flight was canceled, because of a raging storm. Your replacing flight leaves tomorrow morning." one turn is used
+# "You checked-in late to your flight. You'll have to pay a 100€ fee for the manual check-in." 100e will be removed from your account
+# "Your luggage was over weight. The fee for extra kilos is 50e." 50e will be removed from your account
+# "The aircraft underestimated the flight's emissions. The emissions were 15kg higher than expected." 15kg of emissions will be added
+
