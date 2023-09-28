c3abce796e402112c0bff2a3127b228e28b317fe
lummila <lummila.aleksi@gmail.com>
2023-09-28 15:39:01 +0300

    main

--- Projekti1/main.py
+++ Projekti1/main.py
@@ -2,9 +2,23 @@
 
 # SQL-funktiot
 
-#
+# Pelaajan arvojen muuttamisfunktiot
+
+# Sattumafunktiot
+
+# Lentokentälle saapuminen -funktio
+
+# Ohjeet-funktio
+
+# --------------------------------------
+
+# pelaaja = [sijainti 0, massit 1, emissiot 2, kierros 3]
 
 # SQL-yhteys
 
 # main looppi
-# pelaajan input
+# - pelaajan tietojen vetäminen tietokannasta muuttujaan
+# - pelaajan input
+# - ehtolausekkeet sille mitä pelaaja on kirjoittanut
+# - oikean funktion käynnistäminen
+# - pelaajan tietojen uppaaminen
